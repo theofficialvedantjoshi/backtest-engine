7aff50bcbf33c28e5a27a84334a0533db400b4ae
theofficialvedantjoshi <vedant.messi101@gmail.com>
2026-01-08 16:10:42 +0530

    fix: strict ohlc data validation

--- src/models.py
+++ src/models.py
@@ -66,62 +66,47 @@ class Results(BaseModel):
 
 
 class OHLCSchema(pa.DataFrameModel):
+    class Config:
+        strict = True
+
     Date: pa.typing.Index[pd.Timestamp] = pa.Field(coerce=True, unique=True)
-    Open: pa.typing.Series[float] = pa.Field(coerce=True, check_name=True)
-    High: pa.typing.Series[float] = pa.Field(coerce=True, check_name=True)
-    Low: pa.typing.Series[float] = pa.Field(coerce=True, check_name=True)
-    Close: pa.typing.Series[float] = pa.Field(coerce=True, check_name=True)
-    Entry_Signal: pa.typing.Series[int] = pa.Field(
-        nullable=True, default=HOLD, check_name=True
-    )
-    Exit_Signal: pa.typing.Series[int] = pa.Field(
-        nullable=True, default=HOLD, check_name=True
-    )
-    Edit_Signal: pa.typing.Series[int] = pa.Field(
-        nullable=True, default=HOLD, check_name=True
-    )
+    Open: pa.typing.Series[float] = pa.Field(coerce=True)
+    High: pa.typing.Series[float] = pa.Field(coerce=True)
+    Low: pa.typing.Series[float] = pa.Field(coerce=True)
+    Close: pa.typing.Series[float] = pa.Field(coerce=True)
+    Entry_Signal: pa.typing.Series[int] = pa.Field(nullable=True, default=HOLD)
+    Exit_Signal: pa.typing.Series[int] = pa.Field(nullable=True, default=HOLD)
+    Edit_Signal: pa.typing.Series[int] = pa.Field(nullable=True, default=HOLD)
     Signal_Volume: pa.typing.Series[int] = pa.Field(
-        coerce=True, nullable=True, default=0, check_name=True
+        coerce=True, nullable=True, default=0
     )
     Signal_Stop_Loss: pa.typing.Series[float] = pa.Field(
-        coerce=True, nullable=True, default=0.0, check_name=True
+        coerce=True, nullable=True, default=0.0
     )
     Signal_Limit: pa.typing.Series[float] = pa.Field(
-        coerce=True, nullable=True, default=0.0, check_name=True
+        coerce=True, nullable=True, default=0.0
     )
 
 
 class TradesSchema(pa.DataFrameModel):
-    Trade_ID: pa.typing.Index[int] = pa.Field(unique=True, check_name=True)
-    State: pa.typing.Series[str] = pa.Field(isin=["Open", "Closed"], check_name=True)
+    Trade_ID: pa.typing.Index[int] = pa.Field(unique=True)
+    State: pa.typing.Series[str] = pa.Field(isin=["Open", "Closed"])
     Order_Type: pa.typing.Series[str] = pa.Field(
-        isin=[OrderType.BUY.value, OrderType.SELL.value], check_name=True
+        isin=[OrderType.BUY.value, OrderType.SELL.value]
     )
-    Volume: pa.typing.Series[int] = pa.Field(gt=0, check_name=True)
+    Volume: pa.typing.Series[int] = pa.Field(gt=0)
     Open_Time: pa.typing.Series[pd.Timestamp] = pa.Field(check_name=True)
     Open_Price: pa.typing.Series[float] = pa.Field(check_name=True)
-    Close_Time: pa.typing.Series[pd.Timestamp] = pa.Field(
-        nullable=True, check_name=True
-    )
-    Close_Price: pa.typing.Series[float] = pa.Field(nullable=True, check_name=True)
+    Close_Time: pa.typing.Series[pd.Timestamp] = pa.Field(nullable=True)
+    Close_Price: pa.typing.Series[float] = pa.Field(nullable=True)
     Stop_Loss: pa.typing.Series[float] = pa.Field(check_name=True)
     Limit: pa.typing.Series[float] = pa.Field(check_name=True)
-    Info: pa.typing.Series[str] = pa.Field(nullable=True, default="", check_name=True)
-    Profit: pa.typing.Series[float] = pa.Field(
-        nullable=True, coerce=True, check_name=True
-    )
+    Info: pa.typing.Series[str] = pa.Field(nullable=True, default="")
+    Profit: pa.typing.Series[float] = pa.Field(nullable=True, coerce=True)
     Commission: pa.typing.Series[float] = pa.Field(
-        nullable=True, coerce=True, default=0.0, check_name=True
+        nullable=True, coerce=True, default=0.0
     )
-    Net_Profit: pa.typing.Series[float] = pa.Field(
-        nullable=True, coerce=True, check_name=True
-    )
-    Cumulative_Profit: pa.typing.Series[float] = pa.Field(
-        nullable=True, coerce=True, check_name=True
-    )
-    Balance: pa.typing.Series[float] = pa.Field(
-        nullable=True, coerce=True, check_name=True
-    )
-    Drawdown: pa.typing.Series[float] = pa.Field(
-        nullable=True, coerce=True, check_name=True
-    )
+    Net_Profit: pa.typing.Series[float] = pa.Field(nullable=True, coerce=True)
+    Cumulative_Profit: pa.typing.Series[float] = pa.Field(nullable=True, coerce=True)
+    Balance: pa.typing.Series[float] = pa.Field(nullable=True, coerce=True)
+    Drawdown: pa.typing.Series[float] = pa.Field(nullable=True, coerce=True)
